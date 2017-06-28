e646e718cee6be303abf9cc28c92f0aebf7d4fc3
Asmaa Fawzy <asmaafawzy319@gmail.com>
2017-06-29 00:13:18 +0200

    new

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+$(function () {
+    'use strict';
+    $('.info-list li').click(function () {
+        $(this).addClass('active').siblings('li').removeClass('active');
+        $('.info-content div').hide();
+        $('.' + $(this).data('class')).fadeIn();
+    });
+});
